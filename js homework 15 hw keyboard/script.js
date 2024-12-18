dd398f3f3bed411b09216ed58462585858344965
Darknes <riroox@gmail.com>
2024-12-18 15:16:42 +0200

    add hw keyboard

--- js homework 15 hw keyboard/script.js
+++ js homework 15 hw keyboard/script.js
@@ -1,20 +1,44 @@
-const p = document.querySelector('.p')
+
 const inpt = document.querySelector('.inpt')
-const div = document.querySelector('.div')
+const div = document.querySelector('.wrong')
+let i = 0
+const word = 'banana'
+const span = document.querySelectorAll('span')
+let TabI = 0
 
 document.addEventListener('keydown', (event) =>{
-    if (event.key === 'Tab') {
+    if (event.key === 'Tab' && TabI === 0) {
         event.preventDefault()
         inpt.focus()
+        TabI++
+    }else if(event.key === 'Tab' && TabI === 1){
+        event.preventDefault()
+        inpt.blur()
+        TabI -= 1
     }
+
 })
 
 
-inpt.addEventListener('input', (event) =>{
-    if(inpt.value === word[indexWord]){
-        indexWord += 1
+inpt.addEventListener('input', (e) =>{
+    if(span[i].textContent === inpt.value[i]){
+        span[i].style.color = 'green'
+        i++
     }
 })
+
+inpt.addEventListener('keydown', (e) =>{
+     if(e.code !== 'Backspace' && e.key !== span[i].textContent) {
+        div.textContent = Number(div.textContent) + 1
+    }
+})
+
+inpt.addEventListener('input', () =>{
+    if(inpt.value === 'banana'){
+        inpt.style.display = 'none'
+    }
+})
+
 
 
 
